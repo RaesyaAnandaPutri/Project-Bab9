e9e82022c66480f53d03f1ab972bf87e30a108ee
RaesyaAnandaPutri <putriraesyaananda@gmail.com>
2025-08-19 21:58:45 +0700

    Add files via upload

---
+++ Hewan.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pengenalanClass;
+
+/**
+ *
+ * @author user
+ */
+public class Hewan {
+
+    public String jenisKelamin;
+    public int umur;
+    public String warnaBulu;
+
+    // Constructor default
+    public Hewan() {
+    }
+
+    // Constructor dengan parameter
+    public Hewan(String jenisKelamin, int umur, String warnaBulu) {
+        this.jenisKelamin = jenisKelamin;
+        this.umur = umur;
+        this.warnaBulu = warnaBulu;
+    }
+
+    // Function tanpa parameter
+    public String melompat() {
+        String perilaku = "Anjing melompat pagar.";
+        return perilaku;
+    }
+
+    // Function dengan parameter
+    public String melompat(String parameter) {
+        String perilaku = "Anjing melompat " + parameter + ".";
+        return perilaku;
+    }
+
+    // Prosedur tanpa parameter
+    public void memakan() {
+        System.out.println("Anjing sedang memakan daging.");
+    }
+
+    // Prosedur dengan parameter
+    public void memakan(String parameter) {
+        System.out.println("Anjing sedang memakan " + parameter + ".");
+    }
+}
+
+
+
+
+
+
+       
+     
+  
+ 
+
