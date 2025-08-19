1006771fe30444f9900bff5157801c1d746b9f12
RaesyaAnandaPutri <putriraesyaananda@gmail.com>
2025-08-19 21:59:24 +0700

    Add files via upload

--- Hewan.java
+++ Hewan.java
@@ -9,43 +9,43 @@ package pengenalanClass;
  * @author user
  */
 public class Hewan {
-
-    public String jenisKelamin;
-    public int umur;
-    public String warnaBulu;
-
-    // Constructor default
-    public Hewan() {
-    }
-
-    // Constructor dengan parameter
-    public Hewan(String jenisKelamin, int umur, String warnaBulu) {
-        this.jenisKelamin = jenisKelamin;
-        this.umur = umur;
-        this.warnaBulu = warnaBulu;
-    }
-
-    // Function tanpa parameter
-    public String melompat() {
-        String perilaku = "Anjing melompat pagar.";
-        return perilaku;
-    }
-
-    // Function dengan parameter
-    public String melompat(String parameter) {
-        String perilaku = "Anjing melompat " + parameter + ".";
-        return perilaku;
-    }
-
-    // Prosedur tanpa parameter
-    public void memakan() {
-        System.out.println("Anjing sedang memakan daging.");
-    }
-
-    // Prosedur dengan parameter
-    public void memakan(String parameter) {
-        System.out.println("Anjing sedang memakan " + parameter + ".");
-    }
+//
+//    public String jenisKelamin;
+//    public int umur;
+//    public String warnaBulu;
+//
+//    // Constructor default
+//    public Hewan() {
+//    }
+//
+//    // Constructor dengan parameter
+//    public Hewan(String jenisKelamin, int umur, String warnaBulu) {
+//        this.jenisKelamin = jenisKelamin;
+//        this.umur = umur;
+//        this.warnaBulu = warnaBulu;
+//    }
+//
+//    // Function tanpa parameter
+//    public String melompat() {
+//        String perilaku = "Anjing melompat pagar.";
+//        return perilaku;
+//    }
+//
+//    // Function dengan parameter
+//    public String melompat(String parameter) {
+//        String perilaku = "Anjing melompat " + parameter + ".";
+//        return perilaku;
+//    }
+//
+//    // Prosedur tanpa parameter
+//    public void memakan() {
+//        System.out.println("Anjing sedang memakan daging.");
+//    }
+//
+//    // Prosedur dengan parameter
+//    public void memakan(String parameter) {
+//        System.out.println("Anjing sedang memakan " + parameter + ".");
+//    }
 }
 
 
